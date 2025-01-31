89167982fc534622c8318e201f6294ceeea1b3b3
noahTHEscammer <hovdenoah1@gmail.com>
2025-01-31 09:45:06 +0100

    start

---
+++ snake.js
@@ -0,0 +1,13 @@
+//spillBord
+const blockStr = 25
+const rad = 20
+const kollonne = 20
+var spillBord;
+var context;
+
+
+spillBord = document.getElementById("spillBord");
+spillBord.height = rad * blockStr;
+spillBord.width = kollonne * blockStr
+
+
